b158baabe020d71f74c519af0e9522d92f95ed82
Chris Mucheke <kchrismucheke@gmail.com>
2024-06-21 18:58:09 +0300

    Refresh the to a more modern standard

---
+++ includes/class-schedulify-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://github.com/kchrismucheke
+ * @since      1.0.0
+ *
+ * @package    Schedulify
+ * @subpackage Schedulify/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Schedulify
+ * @subpackage Schedulify/includes
+ * @author     Christopher Mucheke <kchrismucheke@gmail.com>
+ */
+class Schedulify_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-schedulify-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://github.com/kchrismucheke
+ * @since      1.0.0
+ *
+ * @package    Schedulify
+ * @subpackage Schedulify/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Schedulify
+ * @subpackage Schedulify/includes
+ * @author     Christopher Mucheke <kchrismucheke@gmail.com>
+ */
+class Schedulify_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
